f6c9322e7d90bc8e51c051aaeb2b3a0dba2a2fd9
myrayzsiza <rayzanitah2@gmail.com>
2026-02-02 12:07:34 +0300

    hhh

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,44 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const inventoryController = require('../controllers/inventoryController')
-
-// Detail view: /inventory/detail/:invId
-router.get('/detail/:invId', inventoryController.getVehicleDetail)
-
-module.exports = router
-router.get('/inv', invController.buildManagementView)
-
-router.get('/inv/add-classification', invController.buildAddClassification)
-router.post('/inv/add-classification',
-  classificationRules(),
-  checkClassificationData,
-  invController.addClassification
-)
-
-const { classificationRules, checkClassificationData } = require('../middleware/classificationValidation')
-const { inventoryRules, checkInventoryData } = require('../middleware/inventoryValidation')
-
-// Classification routes
-router.get('/inv/add-classification', invController.buildAddClassification)
-router.post('/inv/add-classification',
-  classificationRules(),
-  checkClassificationData,
-  invController.addClassification
-)
-
-// Inventory routes
-router.get('/inv/add-inventory', invController.buildAddInventory)
-router.post('/inv/add-inventory',
-  inventoryRules(),
-  checkInventoryData,
-  invController.addInventory
-)
-
-const express = require('express')
-const router = express.Router()
 const invController = require('../controllers/inventoryController')
 const { classificationRules, checkClassificationData } = require('../middleware/classificationValidation')
 const { inventoryRules, checkInventoryData } = require('../middleware/inventoryValidation')
+
+// Detail view: /inventory/detail/:invId
+router.get('/detail/:invId', invController.getVehicleDetail)
 
 // Management view
 router.get('/inv', invController.buildManagementView)
@@ -59,4 +26,4 @@ router.post('/inv/add-inventory',
   invController.addInventory
 )
 
-module.exports = router;
+module.exports = router
