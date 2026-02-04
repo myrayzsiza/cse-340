d949c9391c488fb53abae98c5ca65041f626be2d
myrayzsiza <rayzanitah2@gmail.com>
2026-02-04 12:31:50 +0300

    error

---
+++ controllers/errorController.js
@@ -0,0 +1,22 @@
+// src/controllers/errorController.js
+
+const errorController = {}
+
+// Handle 404 (page not found)
+errorController.handle404 = (req, res, next) => {
+  res.status(404).render("errors/error", {
+    title: "Page Not Found",
+    message: "Sorry, the page you requested does not exist."
+  })
+}
+
+// Handle 500 (server error)
+errorController.handle500 = (err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).render("errors/error", {
+    title: "Server Error",
+    message: "Something went wrong on our end."
+  })
+}
+
+module.exports = errorController
